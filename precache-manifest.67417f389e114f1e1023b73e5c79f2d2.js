5366306d1c9124792a768fad71ece94110efea57
Vincent Stamos <vstamos1@yahoo.com>
2019-08-25 12:49:43 -0400

    Updates

---
+++ precache-manifest.67417f389e114f1e1023b73e5c79f2d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8cde5af633afc64225651b2524a938",
+    "url": "/vstamos1/taskitplus_react.git/index.html"
+  },
+  {
+    "revision": "076993d0c1d1006a75fb",
+    "url": "/vstamos1/taskitplus_react.git/static/js/2.7db5a412.chunk.js"
+  },
+  {
+    "revision": "cda1d6503ea65e01701d",
+    "url": "/vstamos1/taskitplus_react.git/static/js/3.633b8cb0.chunk.js"
+  },
+  {
+    "revision": "cb94f2558f11c686243c",
+    "url": "/vstamos1/taskitplus_react.git/static/js/main.10b18083.chunk.js"
+  },
+  {
+    "revision": "498079843855ae0158ce",
+    "url": "/vstamos1/taskitplus_react.git/static/js/runtime~main.0f11f186.js"
+  }
+]);
